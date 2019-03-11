d5a1696ae26004d9dd82992270dd4f81cb792e32
Petr Smirnov <psmirnov2000@gmail.com>
2019-03-11 10:49:55 -0400

    run now

--- recalculateSlice.R
+++ recalculateSlice.R
@@ -2,10 +2,10 @@ library(PharmacoGxPrivate)
 
 myfn <- list.files("/pfs/input/slices/")
 print(myfn)
-# mybasenm <- basename(myfn)
+mybasenm <- basename(myfn)
 
-# slice <- readRDS(myfn)
+slice <- readRDS(myfn)
 
-# res <- PharmacoGx:::.calculateFromRaw(slice)
+res <- PharmacoGx:::.calculateFromRaw(slice)
 
-# saveRDS(res, file=paste("/pfs/out/", gsub(basename, pattern = ".rds", replacement="_recomp.rds", fixed=TRUE))
+saveRDS(res, file=paste("/pfs/out/", gsub(basename, pattern = ".rds", replacement="_recomp.rds", fixed=TRUE))
